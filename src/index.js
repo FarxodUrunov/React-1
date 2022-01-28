2c9b33da2e4a2c0608f6381e77981be4a2d9edb8
Farxod <urunovfar@gmail.com>
2022-01-28 12:18:02 +0500

    2. application without JSX, react, reactDOM

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-// "use strict";
+"use strict";
+
+// import ReactDOM from "react-dom";
+
+// const element = <h1 title="foo"> Hello </h1>;
+// const container = document.querySelector("#root");
+// ReactDOM.render(element, container);
+
+//? jsx siz ishlash
 
 import ReactDOM from "react-dom";
+import React from "react";
 
-const element = <h1 title="foo"> Hello </h1>;
+const element = React.createElement("h1", { title: "foo" }, "Hello World");
 const container = document.querySelector("#root");
 ReactDOM.render(element, container);
-
