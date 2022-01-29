baae6865dfd58d0347f5a101390fefe1ae0cf32e
Farxod <urunovfar@gmail.com>
2022-01-29 09:25:52 +0500

    6. yes react jsx

--- src/index.js
+++ src/index.js
@@ -17,22 +17,34 @@
 
 // ? without React, jsx
 
+// import ReactDOM from "react-dom";
+
+// const element = {
+//    type: "h1",
+//    props: { title: "foo", children: "Hello" },
+// };
+
+// const container = document.getElementById("root");
+
+// const node = document.createElement(element.type); // h1
+// node.title = element.props.title;
+// // node["title"] = element.props.title; // foo
+// const text = document.createTextNode(""); //
+// // text.nodeValue = ""
+// text["nodeValue"] = element.props.children; // hello
+// node.append(text);
+// container.append(node);
+
+
+// Step1 createElement with jsx
+
 import ReactDOM from "react-dom";
-
-const element = {
-   type: "h1",
-   props: { title: "foo", children: "Hello" },
-};
-
+// import React from "react";
+const element = (
+	<div id="foo">
+		<a>bar</a>
+		<br/>
+	</div>
+)
 const container = document.getElementById("root");
-
-const node = document.createElement(element.type); // h1
-node.title = element.props.title;
-// node["title"] = element.props.title; // foo
-const text = document.createTextNode(""); //
-// text.nodeValue = ""
-text["nodeValue"] = element.props.children; // hello
-node.append(text);
-container.append(node);
-
-// ReactDOM.render(element, container);
+ReactDOM.render(element, container);
